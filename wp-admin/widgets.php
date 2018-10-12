b98285a53893c95c1510643fd8acf0dfbb6e5441
hashnote <golsanarabegumbd@gmail.com>
2018-10-12 22:53:56 +0600

    upload

---
+++ wp-admin/widgets.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://hashnote.github.io/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fwidgets.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://hashnote.github.io/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fwidgets.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://hashnote.github.io/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fwidgets.php&reauth=1">https://hashnote.github.io/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fwidgets.php&reauth=1</a></p>
+	</body>
+</html>
---
+++ wp-login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; hashnote &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='https://hashnote.github.io/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<style type="text/css">h1 a {background-image: url(https://hashnote.github.io/wp-content/themes/dooplay/assets/img/logo_dt.png)!important;background-size: 244px 52px !important;width: 301px !important;height: 52px !important;margin-bottom: 0!important;}body.login {background: #fff;}</style><meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://hashnote.github.io/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="https://hashnote.github.io/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="https://hashnote.github.io/wp-admin/widgets.php" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://hashnote.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://hashnote.github.io/">&larr; Back to hashnote</a></p>
+		
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
