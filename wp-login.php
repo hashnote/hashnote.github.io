208679f8898df9f0470c75965d69e85ce9614650
Hash Note <golsanarabegumbd@gmail.com>
2018-10-18 19:26:12 +0600

    2

--- wp-login.php
+++ wp-login.php
@@ -7,10 +7,22 @@
 	<!--<![endif]-->
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Log In &lsaquo; HashNote &#8212; WordPress</title>
+	<title>Log In &lsaquo; Hashnote &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='https://hashnote.github.io/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
-<style type="text/css">h1 a {background-image: url(https://hashnote.github.io/wp-content/themes/dooplay_2.1.3.96-com-logo/assets/img/logo_dt.png)!important;background-size: 244px 52px !important;width: 301px !important;height: 52px !important;margin-bottom: 0!important;}body.login {background: #fff;}</style><meta name='robots' content='noindex,follow' />
+<link rel='stylesheet' href='https://hashnote.github.io/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<style type="text/css">
+h1 a {
+background-image: url(https://hashnote.github.io/wp-content/themes/DOOPLAY%20ULTIMATE/assets/img/logo_dt.png) !important;
+    background-size: 244px 52px !important;
+    width: 301px !important;
+    height: 52px !important;
+    margin-bottom: 0!important;
+}
+body.login {
+	background: #fff;
+}
+</style>
+<meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
 		</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
@@ -29,7 +41,7 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="https://hashnote.github.io/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://hashnote.github.io/wp-admin/widgets.php" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
@@ -52,7 +64,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="https://hashnote.github.io/">&larr; Back to HashNote</a></p>
+	<p id="backtoblog"><a href="https://hashnote.github.io/">&larr; Back to Hashnote</a></p>
 		
 	</div>
 
