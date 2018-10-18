d4b6fe10b4697108d8ef80804dd820f3bdfc77b7
Hash Note <golsanarabegumbd@gmail.com>
2018-10-18 12:24:50 +0600

    u

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="search-form" action="<?php echo esc_url( home_url('/') ); ?>">
-<input class="search-input" type="text" name="s" placeholder="<?php _d('Search...'); ?>" value="<?php echo get_search_query(); ?>" spellcheck="false">
-<button type="submit" id=""><span class="icon-search2"></span></button>
-</form>
